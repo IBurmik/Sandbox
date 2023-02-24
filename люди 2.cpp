3c1bf3ca34d7ff05a51e8d564cd9fd7aeae8d790
IBurmik <126238944+IBurmik@users.noreply.github.com>
2023-02-24 12:17:58 +0300

    Add files via upload

---
+++ люди 2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include<string>
+using namespace std;
+
+class chelovek
+{
+public:
+	int age;
+	int rost;
+	string name;
+	string surname;
+	void one();
+	void two();
+};
+
+void chelovek::one()
+{
+cout << "введите имя"<<endl;
+	cin >> name;
+	cout << "введите фамилию" << endl;
+	cin >> surname;
+	cout << "введите возраст" << endl;
+	cin >> age;
+	cout << "введите рост" << endl;
+	cin >> rost;
+	
+}
+
+void chelovek::two()
+{
+	cout << name << " " << surname << " " << age << " " << rost<<endl;
+}
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	chelovek a;
+	a.one();
+	a.two();
+	
+	
+}
---
+++ люди.cpp
@@ -0,0 +1,41 @@
+﻿#include<iostream>
+#include <string>
+using namespace std;
+class chelovek
+{
+public:
+	int age;
+	int rost;
+	string name;
+	string surname;
+	
+};
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	chelovek one;
+	cout << "первый человек" << endl;
+	cout << "введите имя" << endl;
+	cin >> one.name;
+	cout << "введите фамилию" << endl;
+	cin >> one.surname;
+	cout << "введите возраст" << endl;
+	cin >> one.age;
+	cout << "введите рост" << endl;
+	cin >> one.rost;
+
+
+	chelovek two;
+	cout <<endl<< "второй человек" << endl ;
+	cout << "введите имя" << endl;
+	cin >> two.name;
+	cout << "введите фамилию" << endl;
+	cin >> two.surname;
+	cout << "введите возраст" << endl;
+	cin >> two.age;
+	cout << "введите рост" << endl;
+	cin >> two.rost;
+
+
+}
